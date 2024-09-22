e7f75761528b8db873cc2966ffce7c586d97a80e
GTPSHAX <rafiehasannudin@gmail.com>
2024-09-22 20:29:14 +0700

    Add key id on search endpoint

--- process/search.js
+++ process/search.js
@@ -13,18 +13,18 @@ addCommand("search", async (data) => {
             if (name.length < 3) {
                 throw new Error("Minimum 3 char name!");
             }
+            return items.items.length;
+            const results = await API.searchItem(name);
 
-            const results = await API.searchItem(name).then(data => {
-                data.forEach(a => {
-                    const foundItem = items.items.find(i => i.name.toLowerCase() ==
-                    a.itemName.toLowerCase());
-                    if (foundItem) {
-                        a.id = foundItem.id;
-                    }
-                });
-                return data;
+            results.forEach(item => {
+                const foundItem = items.items.find(i => i.name.toLowerCase() ==
+                item.itemName.toLowerCase());
+                if (foundItem) {
+                    item.id = foundItem.id
+                }
             });
-            return (results.length ? results : {"System": "Item not found!"});
+
+            return (results.length ? results : { "System": "Item not found!" });
         } else {
             throw new Error("Error processing '/search': Invalid data!");
         }
