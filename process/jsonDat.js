954c9d74011af53ecf58416fb467a4a63a48581d
GTPSHAX <rafiehasannudin@gmail.com>
2024-09-22 21:30:41 +0700

    Add json to dat encoder

--- process/jsonDat.js
+++ process/jsonDat.js
@@ -1,5 +1,15 @@
 const { addCommand } = require("../router/registered");
 
 addCommand("jsonDat", async (data) => {
-    return JSON.parse(data.buffer.toString("utf-8"));
+    try {
+        if (!data.buffer) {
+            throw new Error("No buffer provided.");
+        }
+        
+        const jsonData = JSON.parse(data.buffer.toString("utf-8"));
+
+        return jsonData;
+    } catch (error) {
+        return { "System": `Error processing JSON: ${error.message}` };
+    }
 });
